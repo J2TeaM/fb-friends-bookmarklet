0147a2584684e60e6ea8409e37edc32358e47ca6
J2TeaM <junookyo@gmail.com>
2016-02-11 01:43:37 +0700

    Make sure we have a valid User ID

--- simple-display.js
+++ simple-display.js
@@ -3,35 +3,57 @@
  */
 // ---------------------------------------------------------------------
 (function(w, d) {
+	'use strict';
+
 	function creator(o, data, node) {
 		var content = d.createElement(node);
-		content.cellspacing = "3"
+		content.cellspacing = '3';
 		var cell = d.createTextNode(data);
 		content.appendChild(cell);
 		o.appendChild(content);
 	}
 
 	function displayData(arr) {
-		var table = d.createElement('table');
-		var thead = d.createElement('thead');
+		var table = d.createElement('table'),
+			thead = d.createElement('thead');
+
 		table.appendChild(thead);
+
 		var row = d.createElement('tr');
 		creator(row, 'Name', 'th');
 		creator(row, 'Type', 'th');
 		creator(row, 'Score', 'th');
 		thead.appendChild(row);
+
 		var tbody = d.createElement('tbody');
 		table.appendChild(tbody);
-		for (i = 0; i < arr.length; i++) {
-			var type = arr[i].type;
-			var row = d.createElement('tr');
-			creator(row, arr[i]["text"], 'td');
-			creator(row, Object.keys(arr[i]["grammar_costs"])[0].slice(0, -1).substring(1), 'td');
-			creator(row, arr[i]["grammar_costs"][Object.keys(arr[i]["grammar_costs"])[0]], 'td');
-			tbody.appendChild(row);
+
+		var t = arr.length;
+		for (var i = 0; i < t; i++) {
+			var type = arr[i].type,
+				_row = d.createElement('tr');
+
+			creator(_row, arr[i].text, 'td');
+			creator(_row, Object.keys(arr[i].grammar_costs)[0].slice(0, -1).substring(1), 'td');
+			creator(_row, arr[i].grammar_costs[Object.keys(arr[i].grammar_costs)[0]], 'td');
+			tbody.appendChild(_row);
 		}
-		d.body.innerHTML = "";
+		d.body.innerHTML = '';
 		d.body.appendChild(table);
+	}
+
+	function getUserId() {
+		try {
+			return require('CurrentUserInitialData').USER_ID || require('CurrentUserInitialData').ACCOUNT_ID || require('Env').user || require('CurrentUserInitialData').id;
+		} catch (e) {
+			var ck = d.cookie.match(/c_user=([0-9]+)/);
+			if (ck === null) {
+				return 0;
+			}
+
+			// ["c_user=XXX", "XXX"]
+			return ck[1];
+		}
 	}
 
 	// Make sure the current tab is Facebook
@@ -39,21 +61,21 @@
 		//need to find user's unique id. Trying each of the below until succeeds.
 		//facebook keeps changing their variables and keys
 
-		id = require("CurrentUserInitialData")["USER_ID"] || require("CurrentUserInitialData")["ACCOUNT_ID"] || require("Env").user || require("CurrentUserInitialData")["id"];
+		var id = getUserId();
 
 		// Check login status
 		if (parseInt(id) === 0) {
-			alert('Please login to continue!');
+			console.error('Please login to continue!');
 		} else {
-			url = "//www.facebook.com/ajax/typeahead/search/facebar/bootstrap/?viewer=" + id + "&__a=1";
-			x = new XMLHttpRequest();
+			var url = '//www.facebook.com/ajax/typeahead/search/facebar/bootstrap/?viewer=' + id + '&__a=1',
+				x = new XMLHttpRequest();
+
 			x.onreadystatechange = function() {
 				if (x.readyState === 4 && x.status === 200) {
-					srr = JSON.parse(x.responseText.substr(9)).payload.entries;
-					displayData(srr);
+					displayData(JSON.parse(x.responseText.substr(9)).payload.entries);
 				}
-			}
-			x.open("GET", url, true);
+			};
+			x.open('GET', url, true);
 			x.send();
 		}
 	}
